92becb1eac9f3e99fae6c04aff6477e9841707e7
WataruHayashi1 <wataru.hayashi@hokuden-it.co.jp>
2022-05-17 14:58:07 +0900

    add text TeX

--- scene.py
+++ scene.py
@@ -4,3 +4,28 @@ class SquareToCircle(Scene):
         circle = Circle()
         circle.set_fill(PINK, opacity=0.5)
         self.play(Create(circle))
+
+class ContinuousMotion(Scene):
+    def construct(self):
+        func = lambda pos: np.sin(pos[0] / 2) * UR + np.cos(pos[1] / 2) * LEFT
+        stream_lines = StreamLines(func, stroke_width=2, max_anchors_per_line=30)
+        self.add(stream_lines)
+        stream_lines.start_animation(warm_up=False, flow_speed=1.5)
+        self.wait(stream_lines.virtual_time / stream_lines.flow_speed)
+
+class SingleLineColor(Scene):
+    def construct(self):
+        text = MarkupText(
+            f'all in red <span fgcolor="{YELLOW}">except this</span>', color=RED
+        )
+        self.add(text)
+
+class HelloWorld(Scene):
+    def construct(self):
+        text = Text("Hello world", font_size=144)
+        self.add(text)
+
+class HelloLaTeX(Scene):
+    def construct(self):
+        tex = Tex(r"\LaTeX", font_size=144)
+        self.add(tex)
